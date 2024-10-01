1e5e183772944ecd82ee2a33168aacb36004672c
Forhad <forhad16@nmsu.edu>
2024-10-01 12:31:24 -0500

    updating set up file

--- VectorWrite/setup.sh
+++ VectorWrite/setup.sh
@@ -3,6 +3,7 @@ DIR_BUILD=$DIR_TOP/build
 DIR_INST=$DIR_TOP/inst
 
 source /exp/seaquest/app/software/osg/software/e1039/this-e1039.sh
+#source /project/ptgroup/spinquest/this-e1039.sh
 #source /exp/seaquest/app/software/osg/users/$USER/e1039/core/this-e1039.sh
 export LD_LIBRARY_PATH=$DIR_INST/lib:$LD_LIBRARY_PATH
 
